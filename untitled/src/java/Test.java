6b95317f62867135f774f21573933cb517d21293
yuxiaodong <894807501@qq.com>
2019-09-21 09:50:28 +0800

    修改test

--- untitled/src/java/Test.java
+++ untitled/src/java/Test.java
@@ -1,4 +1,14 @@
 package java;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("java.Test")
+@RequestMapping("/yxd/test")
 public class Test {
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "";
+    }
+
 }
